4b78b5919d40323710eefa15b31428f1bcd3030d
Turuu30 <trmnhtr@gmail.com>
2023-06-06 20:43:13 +0800

    deploy

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -40,8 +40,6 @@ const ComputersCanvas = () => {
     };
   }, []);
 
-  console.log(isMobile);
-
   return (
     <Canvas frameloop="demand" shadows camera={{ position: [20, 3, 5], fov: 25 }} gl={{ preserveDrawingBuffer: true }}>
       <Suspense fallback={<CanvasLoader />}>
